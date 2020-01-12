75d12b5603321717b3b310443d6c651e2ba45bb6
connor paterick <cwpaterick@gmail.com>
2020-01-12 23:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function logWhisper(string){
   console.log('hello')
 }
 function sayHitograndma(string) {
-  if (string === string.toLowerCase()) 
+  if (string === ()) 
     return ("I can't hear you!");
 }
