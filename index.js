63c8aa4bfae90eea47e66bc83a4a1fab85994a5b
connor paterick <cwpaterick@gmail.com>
2020-01-12 23:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === whisper(string)){
     return ("I can't hear you!")}
+    else if (string === shout(string)){
+      return ('YES INDEED!')}
+      else return ('I love you, too.')
+}
